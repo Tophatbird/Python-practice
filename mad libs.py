1d1a1ffd38af67f04582108bcb0334c2bcf7d3b5
Tophatbird <davidavilajr2008@gmail.com>
2024-07-31 08:51:39 -0700

    Add files via upload

---
+++ mad libs.py
@@ -0,0 +1,23 @@
+noun = input("Choose a noun ")
+
+plural = input("Choose a plural noun ")
+
+name = input("Choose a name ")
+
+place = input("Name a place ")
+
+adj = input("choose a adjective ")
+
+
+
+
+
+print("Today I had a " + noun + " for breakfast!")
+
+print(  plural + " are very tasty!")
+
+print(  name + " is kinda anoying.")
+
+print( place + " is my favorite place in the world!")
+
+print("He looks very " + adj + " today!")
